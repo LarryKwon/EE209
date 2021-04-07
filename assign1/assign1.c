b462617034eb5cf23fc87581abcf93f8016584f7
larrykwon <koallarry11@kaist.ac.kr>
2021-04-07 23:47:37 +0900

    DFA setup

--- assign1/assign1.c
+++ assign1/assign1.c
@@ -1,7 +1,60 @@
 #include <stdio.h>
 
+enum DFAState
+{
+    Out,
+    In,
+    PreComment_In,
+    PreComment_Out,
+    Comment,
+    PreOut
+};
+
+typedef struct
+{
+    int nw;
+    int nc;
+    int nl;
+    enum DFAState state;
+} wordCount;
+
 int main()
 {
-    printf("Hello,world");
-    printf("hello,world");
+    wordCount wc;
+    wc.nc = 0;
+    wc.nw = 0;
+    wc.nl = 0;
+    wc.state = Out;
+    int c;
+    while (c = getchar() != EOF)
+    {
+        switch (wc.state)
+        {
+        case Out:
+
+            break;
+
+        case In:
+
+            break;
+
+        case PreComment_In:
+
+            break;
+
+        case PreComment_Out:
+
+            break;
+
+        case Comment:
+
+            break;
+
+        case PreOut:
+
+            break;
+        }
+
+        printf("%d %d %d", nl, nw, nc);
+    }
 }
