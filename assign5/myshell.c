8eb9858a64f01bdca076221b7ef6d6eb40eab922
larrykwon <koallarry11@kaist.ac.kr>
2021-05-22 18:32:02 +0900

    debugging errors which is subtle

--- assign5/myshell.c
+++ assign5/myshell.c
@@ -37,7 +37,8 @@ typedef int (*BuiltIn_T)(DynArray_T);
 
 static DynArray_T executionInit(DynArray_T oTokens, char *acLine)
 {
-    int isSuccessful;
+    int lexical;
+    int syntatic;
     int valid;
 
     oTokens = DynArray_new(0);
@@ -47,20 +48,25 @@ static DynArray_T executionInit(DynArray_T oTokens, char *acLine)
         exit(EXIT_FAILURE);
     }
 
-    isSuccessful = lexLine(acLine, oTokens);
-    if (isSuccessful)
+    lexical = lexLine(acLine, oTokens);
+    if (lexical)
     {
-        isSuccessful = syntaticLine(oTokens);
-        DynArray_map(oTokens, printAnyTokenWithTokenType, NULL);
-    }
-    if (isSuccessful)
-    {
-        printf("%s\n", "valid");
-        DynArray_map(oTokens, printAnyTokenWithCommandType, NULL);
+        syntatic = syntaticLine(oTokens);
+        // DynArray_map(oTokens, printAnyTokenWithTokenType, NULL);
     }
     else
     {
-        printf("%s\n", "invalid");
+        return NULL;
+    }
+    if (syntatic)
+    {
+        //printf("%s\n", "valid");
+        //DynArray_map(oTokens, printAnyTokenWithCommandType, NULL);
+    }
+    else
+    {
+        // printf("%s\n", "invalid");
+        return NULL;
     }
     return oTokens;
 }
@@ -71,7 +77,7 @@ int execCd(DynArray_T oTokens)
 
     if (commandLine > 2)
     {
-        fprintf(stderr, "cd Takes one parameter");
+        fprintf(stderr, "cd Takes one parameter\n");
         return FALSE;
     }
     else if (commandLine == 1)
@@ -107,26 +113,29 @@ int execCd(DynArray_T oTokens)
 int execSetenv(DynArray_T oTokens)
 {
     int commandLine = DynArray_getLength(oTokens);
-    enum CommandType cType = getCommandType(DynArray_get(oTokens, 1));
     if (commandLine > 3 || commandLine == 1)
     {
-        fprintf(stderr, "setenv Takes one or two parameter");
-        return FALSE;
-    }
-    else if (cType == STDIN || cType == STDOUT || cType == PIPE)
-    {
-        fprintf(stderr, "setenv takes one or two parameter");
+        fprintf(stderr, "setenv Takes one or two parameter\n");
         return FALSE;
     }
     else
     {
-        char *firstParam = getTokenValue(DynArray_get(oTokens, 1));
-        char *secondParam = getTokenValue(DynArray_get(oTokens, 2));
-        int result = setenv(firstParam, secondParam, 1);
-        if (result == -1)
+        enum CommandType cType = getCommandType(DynArray_get(oTokens, 1));
+        if (cType == STDIN || cType == STDOUT || cType == PIPE)
         {
-            fprintf(stderr, "%s\n", strerror(errno));
+            fprintf(stderr, "setenv takes one or two parameter\n");
             return FALSE;
+        }
+        else
+        {
+            char *firstParam = getTokenValue(DynArray_get(oTokens, 1));
+            char *secondParam = getTokenValue(DynArray_get(oTokens, 2));
+            int result = setenv(firstParam, secondParam, 1);
+            if (result == -1)
+            {
+                fprintf(stderr, "%s\n", strerror(errno));
+                return FALSE;
+            }
         }
     }
     return TRUE;
@@ -137,7 +146,7 @@ int execUnsetenv(DynArray_T oTokens)
     int commandLine = DynArray_getLength(oTokens);
     if (commandLine != 2)
     {
-        fprintf(stderr, "unsetenv Takes one parameter");
+        fprintf(stderr, "unsetenv Takes one parameter\n");
         return FALSE;
     }
     else
@@ -158,7 +167,7 @@ int execExit(DynArray_T oTokens)
     int commandLine = DynArray_getLength(oTokens);
     if (commandLine != 1)
     {
-        fprintf(stderr, "exit does not take any parameters");
+        fprintf(stderr, "exit does not take any parameters\n");
         return FALSE;
     }
     else
@@ -241,18 +250,29 @@ int execute()
 int main(int argc, char *argv[])
 {
     char acLine[MAX_LINE_SIZE];
-    printf("%%");
+    printf("%% ");
     while (fgets(acLine, MAX_LINE_SIZE, stdin) != NULL)
     {
         DynArray_T oTokens;
         oTokens = executionInit(oTokens, acLine);
+
+        if (oTokens == NULL)
+        {
+            printf("%% ");
+            continue;
+        }
 
         enum BuiltInCommand builtInCommand = isBuiltIn(oTokens);
 
         if (builtInCommand != NOTHING)
         {
             BuiltIn_T builtInFunction = BuiltInContext(builtInCommand);
-            (*builtInFunction)(oTokens);
+            int builtInResult = (*builtInFunction)(oTokens);
+            if (builtInResult == FALSE)
+            {
+                printf("%%");
+                continue;
+            }
         }
         else
         {
